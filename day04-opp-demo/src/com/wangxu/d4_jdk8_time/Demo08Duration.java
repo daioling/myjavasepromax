8429976ac8c9c8bd711146d9b67c7d41eb05b643
29226 <2922674369@qq.com>
2023-05-05 11:05:54 +0800

    日期API

--- day04-opp-demo/src/com/wangxu/d4_jdk8_time/Demo08Duration.java
+++ day04-opp-demo/src/com/wangxu/d4_jdk8_time/Demo08Duration.java
@@ -1,5 +1,6 @@
 package com.wangxu.d4_jdk8_time;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class Demo08Duration {
@@ -9,5 +10,13 @@ public class Demo08Duration {
         System.out.println(today);
         //出生日期时间对象
         LocalDateTime birthDate = LocalDateTime.of(2000, 2, 22,20,00,00);
+        System.out.println(birthDate);
+        Duration duration = Duration.between(birthDate, today);
+        System.out.println(duration.toDays()); // 两个时间差的天数
+        System.out.println(duration.toHours()); //两个时间差的小时数
+        System.out.println(duration.toMinutes());//两个时间差的分钟数
+        System.out.println(duration.toMillis());//两个时间差的毫秒数
+        System.out.println(duration.toNanos());//两个时间差的纳秒数
+
     }
 }
